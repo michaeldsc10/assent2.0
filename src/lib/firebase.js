1471f6824c0c48f6d24910d77a4e43022f75d779
michaeldsc10 <michaeldsc10@gmail.com>
2026-04-20 10:37:05 -0300

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -33,6 +33,13 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
+export const firebaseConfig = { apiKey: "AIzaSyB9HEWiHFc8YEuj_Ab-7TxGKqdQkSRQAio",
+  authDomain: "assent-2b945.firebaseapp.com",
+  projectId: "assent-2b945",
+  storageBucket: "assent-2b945.firebasestorage.app",
+  messagingSenderId: "851051401705",
+  appId: "1:851051401705:web:fa6ebb1cc6ee5d3a737b78",
+  measurementId: "G-K7F0F7PZ8M" };
 
 /* ── Funções de Autenticação ── */
 export const login = (email, password) => 
